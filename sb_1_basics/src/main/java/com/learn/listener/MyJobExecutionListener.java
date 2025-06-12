ac1aed4b5a7bd2a77a11f5364a97721abc525611
Shashi7083 <shashikr12047@gmail.com>
2025-06-12 21:42:13 +0530

    another way to implement jobExecutionListener

--- sb_1_basics/src/main/java/com/learn/listener/MyJobExecutionListener.java
+++ sb_1_basics/src/main/java/com/learn/listener/MyJobExecutionListener.java
@@ -2,18 +2,21 @@ package com.learn.listener;
 
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.annotation.AfterJob;
+import org.springframework.batch.core.annotation.BeforeJob;
 
-public class MyJobExecutionListener implements JobExecutionListener {
+public class MyJobExecutionListener {
+//implements JobExecutionListener {
 
 		
-	@Override
+	@BeforeJob
 	public void beforeJob(JobExecution jobExecution) {
 		System.out.println("jobName : "+jobExecution.getJobInstance().getJobName());
 		System.out.println("Job Parameters: "+jobExecution.getJobParameters());
 		System.out.println("Job Start Time: "+jobExecution.getStartTime());
 	}
 
-	@Override
+	@AfterJob
 	public void afterJob(JobExecution jobExecution) {
 		System.out.println("jobName : "+jobExecution.getJobInstance().getJobName());
 		System.out.println("Job Parameters: "+jobExecution.getJobParameters());
